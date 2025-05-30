d5efce31bd824a68489a32e9a30a2efe8466c21b
Zeke Sikelianos <zeke@sikelianos.com>
2025-05-30 12:42:40 -0700

    basic hono app with react and tailwind

---
+++ public/app.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="w-full max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
+      <div className="prose prose-neutral">
+        <h1>Hello, React!</h1>
+        <p>This is a barebones React app using CDN-hosted dependencies.</p>
+      </div>
+    </main>
+  );
+} 
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import Replicate from 'replicate';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Env }>();
+
+app.post('/generate-image', async (c) => {
+	const replicate = new Replicate({auth: c.env.REPLICATE_API_TOKEN})
+	const model = 'black-forest-labs/flux-schnell'  
+	const prompt = await c.req.text()
+	const output = await replicate.run(model, {
+	  input: {
+		prompt,
+		image_format: 'webp',
+	  }
+	})
+	  
+	// Some image models return an array of output files, others just a single file.
+	const imageUrl = Array.isArray(output) ? output[0].url() : output.url()
+   
+	console.log({imageUrl})
+
+	return c.body(imageUrl, {
+		headers: {
+			'Content-Type': 'image/webp',
+		},
+	});
+});
+
+export default app;
